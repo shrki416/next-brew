86dd6d90781e966815992448c8a20b0dfcab5528
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-11 15:17:21 -0500

    Style homepage

--- components/Banner.js
+++ components/Banner.js
@@ -4,8 +4,8 @@ export default function Banner(props) {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Brew</span>
-        <span className={styles.title2}>Connisseur</span>
+        <span className={styles.title1}>🍺 Brew</span>
+        <span className={styles.title2}>Master</span>
       </h1>
       <p className={styles.subTitle}>Discover your local breweries!</p>
       <button className={styles.button} onClick={props.handleClick}>
