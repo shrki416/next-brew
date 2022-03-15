11f734a68939621fbdcb89114f6e753a53f02517
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-15 14:16:36 -0400

    Abandon using a map for now

--- pages/brewery/[id].js
+++ pages/brewery/[id].js
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react'
-
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -40,54 +38,39 @@ export async function getStaticPaths() {
 
 export default function Brewery(props) {
   console.log(`🍏`, props.brewery)
-  const map = useRef(null)
-  const mapContainer = useRef(null)
 
   const router = useRouter()
   const { id } = router.query
 
-  useEffect(() => {
-    if (map.current) return
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [props.brewery.longitude, props.brewery.latitude],
-      zoom: 12,
-    })
-  }, [])
-
   return (
-    <div className={styles.container}>
-      <div className={styles.col1}>
-        <div className={styles.backToHomeLink}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-        <div className={styles.nameWrapper}>
-          <h1 className={styles.name}>{props.brewery.name}</h1>
-        </div>
-        {/* <div>
-          <div>
-            <div className="sidebar">
-              Longitude: {props.brewery.longitude} | Latitude:{' '}
-              {props.brewery.latitude} | Zoom: 9
-            </div>
-            <div ref={mapContainer} className="map-container" />
+    <>
+      <Head>
+        <title>{props.brewery.name}</title>
+        <meta name="description" content={props.brewery.description} />
+      </Head>
+
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
           </div>
-        </div> */}
-        <Image
-          src={
-            props.brewery.image ||
-            'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
-          }
-          width={600}
-          height={360}
-          className={styles.storeImg}
-          alt={props.brewery.name}
-        ></Image>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{props.brewery.name}</h1>
+          </div>
+          <Image
+            src={
+              props.brewery.image ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={props.brewery.name}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
