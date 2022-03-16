89f13a32b13101f5d2cc0a36fe838610151398bc
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-16 15:10:16 -0400

    Abandon using a map for now

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
