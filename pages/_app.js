41f572a0966e4ffd2e5fa2f094fd773c1ba9b7b6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-17 12:39:40 -0400

    Implement context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
 import '../styles/globals.css'
 
+import { createContext, useReducer } from 'react'
+
+export const StoreContext = createContext()
+
+export const ACTION_TYPES = {
+  SET_LAT_LONG: 'SET_LAT_LONG',
+  SET_BREWERIES: 'SET_BREWERIES',
+}
+
+export const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG:
+      return {
+        ...state,
+        latLong: action.payload.latLong,
+      }
+    case ACTION_TYPES.SET_BREWERIES:
+      return {
+        ...state,
+        breweries: action.payload.breweries,
+      }
+    default:
+      return state
+  }
+}
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: '',
+    breweries: [],
+  }
+
+  const [state, dispatch] = useReducer(storeReducer, initialState)
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
 }
 
 export default MyApp
