702689da4d81486c9865111fcaacde7b3a7b56fe
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-17 12:39:22 -0400

    Get data from context

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import { ACTION_TYPES, StoreContext } from './_app'
+import { useContext, useEffect, useState } from 'react'
+
 import Banner from '../components/Banner'
 import Card from '../components/Card'
 import Head from 'next/head'
@@ -5,6 +8,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { fetchBreweries } from '../lib/brew'
 import styles from '../styles/Home.module.css'
+import useTrackLocation from '/hooks/useTrackLocation'
 
 export async function getStaticProps() {
   const breweries = await fetchBreweries()
@@ -17,10 +21,33 @@ export async function getStaticProps() {
 }
 
 export default function Home(props) {
-  console.log(props.breweries)
+  // const [brewery, setBreweries] = useState('')
+  const [error, setError] = useState(null)
+  const { handleTrackLocation, locationErrorMsg, loading } = useTrackLocation()
+
+  const { dispatch, state } = useContext(StoreContext)
+  const { breweries, latLong } = state
+
+  useEffect(async () => {
+    if (latLong) {
+      try {
+        const fetchedBreweries = await fetchBreweries(latLong, 18)
+        // setBreweries(fetchedBreweries)
+        dispatch({
+          type: ACTION_TYPES.SET_BREWERIES,
+          payload: {
+            breweries: fetchedBreweries,
+          },
+        })
+      } catch (error) {
+        setError(error.message)
+        console.log(error)
+      }
+    }
+  }, [latLong])
 
   const handleOnBannerBtnClick = () => {
-    console.log('Hi, banner button')
+    handleTrackLocation()
   }
 
   return (
@@ -36,12 +63,34 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <Banner
-          buttonText="View breweries nearby"
+          buttonText={loading ? 'Locating...' : 'View breweries nearby'}
           handleClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+        {error && <p>Something went wrong: {error}</p>}
+
+        {breweries.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Breweries near me</h2>
+            <div className={styles.cardLayout}>
+              {breweries.map((brewery) => (
+                <Card
+                  key={brewery.id}
+                  name={brewery.name}
+                  imgUrl={
+                    brewery.image ||
+                    'https://images.unsplash.com/photo-1495399396117-a3763646f854?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                  }
+                  href={`/brewery/${brewery.id}`}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
         {props.breweries && props.breweries.length > 0 && (
           <div className={styles.sectionWrapper}>
-            <h2 className={styles.heading2}>New York State Breweries</h2>
+            <h2 className={styles.heading2}>New York Breweries</h2>
             <div className={styles.cardLayout}>
               {props.breweries.map((brewery) => (
                 <Card
