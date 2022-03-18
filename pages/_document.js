19302f92827fd5849967c73b9f36506793c965df
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-18 10:19:59 -0400

    Add Font to documents file

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,24 @@
 // pages/_document.js
-import { Head, Html, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
