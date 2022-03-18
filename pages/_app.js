46b0bd0e8a4a161651946f880412d3b53b52b7df
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-03-18 10:18:30 -0400

    Refactor and clean-up

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,6 @@
 import '../styles/globals.css'
 
-import { createContext, useReducer } from 'react'
-
-export const StoreContext = createContext()
-
-export const ACTION_TYPES = {
-  SET_LAT_LONG: 'SET_LAT_LONG',
-  SET_BREWERIES: 'SET_BREWERIES',
-}
-
-export const storeReducer = (state, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.SET_LAT_LONG:
-      return {
-        ...state,
-        latLong: action.payload.latLong,
-      }
-    case ACTION_TYPES.SET_BREWERIES:
-      return {
-        ...state,
-        breweries: action.payload.breweries,
-      }
-    default:
-      return state
-  }
-}
-
-const StoreProvider = ({ children }) => {
-  const initialState = {
-    latLong: '',
-    breweries: [],
-  }
-
-  const [state, dispatch] = useReducer(storeReducer, initialState)
-
-  return (
-    <StoreContext.Provider value={{ state, dispatch }}>
-      {children}
-    </StoreContext.Provider>
-  )
-}
+import StoreProvider from '../store/brew-store'
 
 function MyApp({ Component, pageProps }) {
   return (
